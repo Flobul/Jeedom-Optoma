de98c623dd2d7012038f0be28b6b0bf1951860ae
Flobul <46993341+Flobul@users.noreply.github.com>
2019-02-19 11:58:12 +0100

    Update Optoma.js

--- desktop/js/Optoma.js
+++ desktop/js/Optoma.js
@@ -34,10 +34,6 @@ $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder:
 $("#table_info").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 
 
-$('.eqLogicAttr[data-l2key=askWebParsing]').on('click',function(){
-  $('#option_cgi').css({visibility: "visible"});
-});
-
 $('.eqLogicAttr[data-l2key=askCGI]').on('click',function(){
   $('#option_cgi').css({visibility: "visible"});
 });
@@ -50,42 +46,6 @@ $('.eqLogicAttr[data-l2key=askPJLink]').on('click',function(){
   $('#option_cgi').css({visibility: "hidden"});
 });
 
-
-$('#bt_amxDeviceDiscovery').on('click', function () {
-  			$('#div_alert').showAlert({message: '{{Recherche de l\'équipement en cours. (environ 1 minute)}}', level: 'warning'});
-            $.ajax({
-                type: "POST",
-                url: "plugins/Optoma/core/ajax/Optoma.ajax.php", 
-                data: {
-                    action: "amxDeviceDiscovery",
-                },
-                dataType: 'json',
-                global: false,
-                error: function (request, status, error) {
-                    handleAjaxError(request, status, error);
-                },
-                success: function (data) {
-                    if (data.state != 'ok') {
-                        $('#div_alert').showAlert({message: data.result, level: 'danger'});
-                        return;
-                    }
-					if ($('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value() != '') {
-                          bootbox.confirm('{{Voulez-vous écraser l\'adresse IP actuelle ?}}', function (result) {
-                                  if (result) {
-										$('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value(data.result);
-                                        $('#div_alert').showAlert({message: 'Vidéoprojecteur trouvé ! Veuillez sauvegarger.', level: 'success'});
-                                  }
-                          });
-                    }
-                  	else {
-                        $('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value(data.result);
-                        $('#div_alert').showAlert({message: 'Vidéoprojecteur trouvé ! Veuillez sauvegarder.', level: 'success'});
-                        }
-					}
-	});
-});
-
-
 $('#bt_weboptoma').on('click', function () {
   var nodeId = $('#idipoptoma').value();
   $('#md_modal').dialog({title: "{{Interface Optoma}}"});
@@ -95,6 +55,38 @@ $('#bt_weboptoma').on('click', function () {
 $('#bt_searchCGILink').on('click', function () {
 	searchCGILink();
 });
+
+function amxDeviceDiscovery(_state) {
+	$.ajax({
+		type: "POST",
+		url: "plugins/Optoma/core/ajax/Optoma.ajax.php", 
+		data: {
+			action: "amxDeviceDiscovery",
+			state: _state,
+		},
+		dataType: 'json',
+		global: false,
+		error: function (request, status, error) {
+			handleAjaxError(request, status, error);
+		},
+		success: function (data) {
+			if (data.state != 'ok') {
+				$('#div_alert').showAlert({message: data.result, level: 'danger'});
+				return;
+			}
+			if ($('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value() != '') {
+				bootbox.confirm('{{Voulez-vous écraser l\'adresse IP actuelle ?}}', function (result) {
+				if (result) {
+					$('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value(data.result);
+				}
+				});
+			}
+			else {
+				$('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value(data.result);
+			}
+		}
+	});
+}
 
 function webOptoma() {
   		if ($('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value() != '') {
@@ -118,7 +110,10 @@ function webOptoma() {
 }
 
 function searchCGILink() {
-  		if ($('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value() != '') {
+		$AdrIP = $('.eqLogicAttr[data-l1key=configuration][data-l2key=AdrIP]').value();
+  		$CGILink = $('.eqLogicAttr[data-l1key=configuration][data-l2key=ControlCGI]').value();
+  
+  		if ($AdrIP != ''){
 			$('#div_alert').showAlert({message: '{{Recherche du lien CGI en cours. (environ 15 secondes)}}', level: 'warning'});
             $.ajax({
                 type: "POST",
@@ -136,7 +131,7 @@ function searchCGILink() {
                         $('#div_alert').showAlert({message: data.result, level: 'danger'});
                         return;
                     }
-					if ($('.eqLogicAttr[data-l1key=configuration][data-l2key=ControlCGI]').value() != '' && $('.eqLogicAttr[data-l1key=configuration][data-l2key=ControlCGI]').value() != data.result) {
+					if ($CGILink != '' && $CGILink != data.result) {
                          bootbox.confirm('{{Voulez-vous écraser le lien actuel ?}}', function (result) {
                                if (result) {
                                     $('.eqLogicAttr[data-l1key=configuration][data-l2key=ControlCGI]').value(data.result);
@@ -144,12 +139,16 @@ function searchCGILink() {
                                }
                           });
                     }
-					if ($('.eqLogicAttr[data-l1key=configuration][data-l2key=ControlCGI]').value() == data.result) {
+					else if ($CGILink == data.result) {
 						$('#div_alert').showAlert({message: '{{Lien identique.}}', level: 'warning'});
                     }
-                  	else {
+                  	else if ($CGILink == '') {
                         $('.eqLogicAttr[data-l1key=configuration][data-l2key=ControlCGI]').value(data.result);
                         $('#div_alert').showAlert({message: 'Lien CGI trouvé ! Veuillez sauvegarder.', level: 'success'});
+                    }
+					else {
+                      $('#div_alert').showAlert({message: data.result, level: 'danger'});
+                      return;
                     }
                 }
             });
@@ -175,36 +174,35 @@ function searchCGILink() {
 	}
 	
 	var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
-    if(!isset(_cmd.type) || _cmd.type == 'info' ){
-      tr += '<td width="200px">';
-  		tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
-  		tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 180px;" placeholder="{{Nom}}">';
-  		tr += '<select class="cmdAttr form-control tooltips input-sm" data-l1key="value" style="display : none;margin-top : 5px;margin-right : 10px;" title="La valeur de la commande vaut par défaut la commande">';
-  		tr += '<option value="">Aucune</option>';
-  		tr += '</select>';
-  		tr += '</td>';
-		
-      tr += '<td width="150px">';
-      tr += '<input class="cmdAttr form-control type input-sm" data-l1key="type" value="action" disabled style="margin-bottom : 5px;" />';
-      tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
-  		tr += '</td>';
-
-      tr += '<td>';
-  		tr += '<input class="cmdAttr form-control input-sm" disabled style="margin-bottom : 5px; display : inline-block;" />';
-  		tr += '</td>';
-    
+  if (init(_cmd.type) == 'info') {
 		tr += '<td>';
-		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" unchecked/>{{Historiser}}</label><br/></span> ';
-		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/>{{Afficher}}</label></span> '; 
+		tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
+		tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom}} style="margin-top : 5px" >';
+		tr += '<select class="cmdAttr form-control tooltips input-sm" data-l1key="value" style="margin-top : 5px;margin-right : 10px;" title="La valeur de la commande vaut par défaut la commande" >';
+		tr += '<option value="">Aucune</option>';
+		tr += '</select>';
+		tr += '</td>';
+		tr += '<td>';
+		tr += '<input class="cmdAttr form-control type input-sm" data-l1key="type" value="info" disabled style="margin-top : -5px;width : 40%; display : inline-block;" />';
+		tr += '<span class="subType" subType="' + init(_cmd.subType) + '" style="margin-top : -5px;width : 50%; display : inline-block;" ></span>';
+		tr += '</td>';
+		tr += '<td>';
+  		tr += '<span class="cmdAttr"  data-l1key="configuration" data-l2key="value"></span>';
+		tr += '</td>';
+		tr += '<td>';
+		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> ';
+		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></span></br> ';
+		tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="Min" title="Min" style="width : 30%; display : inline-block;margin-top : 5px;margin-right : 5px;" >';
+		tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="Max" title="Max" style="width : 30%; display : inline-block;margin-top : 5px;margin-right : 5px;" >';
 		tr += '</td>';
 
-    tr += '<td>';
+		tr += '<td>';
     if (is_numeric(_cmd.id)) {
-        tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible tooltips" title="Configuration de la commade" data-action="configure"><i class="fa fa-cogs"></i></a> ';
-        tr += '<a class="btn btn-default btn-xs cmdAction tooltips" title="Test de la commande" data-action="test"><i class="fa fa-rss"></i></a>';
-        tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" title="Supprimer de la commande" data-action="remove"></i></td>';
-    }
-    tr += '</td>';
+		tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible tooltips" title="Configuration de la commande" data-action="configure"><i class="fa fa-cogs"></i></a> ';
+		tr += '<a class="btn btn-default btn-xs cmdAction tooltips" title="Test de la commande" data-action="test"><i class="fa fa-rss"></i></a>';
+		tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" title="Supprimer de la commande" data-action="remove"></i></td>';
+		}
+		tr += '</td>';
 
     $('#table_info tbody').append(tr);
     $('#table_info tbody tr:last').setValues(_cmd, '.cmdAttr');
@@ -212,36 +210,29 @@ function searchCGILink() {
 	if (init(_cmd.type) == 'action') {
 		tr += '<td>';
 		tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
-		tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">';
-		tr += '<select class="cmdAttr form-control tooltips input-sm" data-l1key="value" style="display : none;margin-top : 5px;margin-right : 10px;" title="La valeur de la commande vaut par défaut la commande">';
+		tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="margin-top : 0;width : 50%; display : inline-block;" placeholder="{{Nom}}" >';
+		tr += '<select class="cmdAttr form-control tooltips input-sm" data-l1key="value" style="margin-top : 0;width : 50%; display : inline-block;" title="La valeur de la commande vaut par défaut la commande">';
 		tr += '<option value="">Aucune</option>';
 		tr += '</select>';
 		tr += '</td>';
-		
 		tr += '<td>';
-    tr += '<input class="cmdAttr form-control type input-sm" data-l1key="type" value="action" disabled style="margin-bottom : 1px;" />';
-		tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
+		tr += '<input class="cmdAttr form-control type input-sm" data-l1key="type" value="info" disabled style="margin-top : -5px;width : 40%; display : inline-block;" />';
+		tr += '<span class="subType" subType="' + init(_cmd.subType) + '" style="margin-top : -5px;width : 50%; display : inline-block;" ></span>';
 		tr += '</td>';
-
 		tr += '<td>';
-		tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="infoName" placeholder="Nom information" style="margin-bottom : 5px;width : 70%; display : inline-block;" />';
-		tr += '<a class="btn btn-default btn-sm cursor listEquipementInfo" data-input="infoName" style="margin-left : 5px;"><i class="fa fa-list-alt "></i> Rechercher équipement</a>';
-		tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="value" placeholder="Valeur" style="margin-bottom : 5px;width : 50%; display : inline-block;" />';
-		tr += '<a class="btn btn-default btn-sm cursor listEquipementInfo" data-input="value" style="margin-left : 5px;"><i class="fa fa-list-alt "></i> Rechercher équipement</a>';
+  		tr += '<span class="cmdAttr"  data-l1key="configuration" data-l2key="value"></span>';
 		tr += '</td>';
-		
 		tr += '<td>';
-		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" unchecked/>{{Historiser}}</label><br/></span> ';
-		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/>{{Afficher}}</label></span> '; 
+		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> ';
+		tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></span> ';
 		tr += '</td>';
-
-    tr += '<td>';
+		tr += '<td>';
     if (is_numeric(_cmd.id)) {
-        tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible tooltips" title="Configuration de la commade" data-action="configure"><i class="fa fa-cogs"></i></a> ';
-        tr += '<a class="btn btn-default btn-xs cmdAction tooltips" title="Test de la commande" data-action="test"><i class="fa fa-rss"></i></a>';
-        tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" title="Supprimer de la commande" data-action="remove"></i></td>';
+		tr += '<a class="btn btn-default btn-xs cmdAction expertModeVisible tooltips" title="Configuration de la commande" data-action="configure"><i class="fa fa-cogs"></i></a> ';
+		tr += '<a class="btn btn-default btn-xs cmdAction tooltips" title="Test de la commande" data-action="test"><i class="fa fa-rss"></i></a>';
+		tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" title="Supprimer de la commande" data-action="remove"></i></td>';
     }
-    tr += '</td>';
+		tr += '</td>';
     
     $('#table_cmd tbody').append(tr);
     $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
@@ -250,11 +241,10 @@ function searchCGILink() {
     tr += '</tr>';
 	
 	if (init(_cmd.type) == 'info') {
-    var tr = $('#table_info tbody tr:last');
 		if (isset(_cmd.type)) {
 			$('#table_info tbody tr:last .cmdAttr[data-l1key=type]').value(init(_cmd.type));
 		}
-		jeedom.cmd.changeType(tr, init(_cmd.subType));
+		jeedom.cmd.changeType($('#table_info tbody tr:last'), init(_cmd.subType));
 	}
 	if (init(_cmd.type) == 'action') {
     var tr = $('#table_cmd tbody tr:last');
@@ -266,7 +256,7 @@ function searchCGILink() {
 			},
 			success: function (result) {
 				tr.find('.cmdAttr[data-l1key=value]').append(result);
-				tr.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdId]').append(result);
+				tr.find('.cmdAttr[data-l1key=configuration][data-l2key=value]').append(result);
 				tr.setValues(_cmd, '.cmdAttr');
 				jeedom.cmd.changeType(tr, init(_cmd.subType));
 			}
@@ -274,5 +264,47 @@ function searchCGILink() {
 	}
 }
 
+$('.changeIncludeState').on('click', function () {
+	var el = $(this);
+	var state = $(this).attr('data-state');
+	amxDeviceDiscovery(state);
+	jeedom.config.save({
+		plugin : 'Optoma',
+		configuration: {include_mode: el.attr('data-state')},
+		error: function (error) {
+			$('#div_alert').showAlert({message: error.message, level: 'danger'});
+		},
+		success: function () {
+			if (el.attr('data-state') == 1) {
+				$.hideAlert();
+				$('.changeIncludeState:not(.card)').removeClass('btn-default').addClass('btn-success');
+				$('.changeIncludeState').attr('data-state', 0);
+				$('.changeIncludeState.card').css('background-color','#8000FF');
+				$('.changeIncludeState.card span center').text('{{Arrêter l\'inclusion}}');
+				$('.changeIncludeState:not(.card)').html('<i class="fa fa-sign-in fa-rotate-90"></i> {{Arreter inclusion}}');
+				$('#div_inclusionAlert').showAlert({message: '{{Vous êtes en mode inclusion. Cliquez à nouveau sur le bouton d\'inclusion pour sortir de ce mode}}', level: 'warning'});
+			} else {
+				$.hideAlert();
+				$('.changeIncludeState:not(.card)').addClass('btn-default').removeClass('btn-success btn-danger');
+				$('.changeIncludeState').attr('data-state', 1);
+				$('.changeIncludeState:not(.card)').html('<i class="fa fa-sign-in fa-rotate-90"></i> {{Mode inclusion}}');
+				$('.changeIncludeState.card span center').text('{{Mode inclusion}}');
+				$('.changeIncludeState.card').css('background-color','#ffffff');
+				$('#div_inclusionAlert').hideAlert();
+			}
+		}
+	});
+});
 
+$('body').on('Optoma::includeDevice', function (_event,_options) {
+  if (modifyWithoutSave) {
+    $('#div_inclusionAlert').showAlert({message: '{{Un périphérique vient d\'être inclu/exclu. Veuillez réactualiser la page}}', level: 'warning'});
+  } else {
+    if (_options == '') {
+      window.location.reload();
+    } else {
+      window.location.href = 'index.php?v=d&p=Optoma&m=Optoma&id=' + _options;
+    }
+  }
+});
 
