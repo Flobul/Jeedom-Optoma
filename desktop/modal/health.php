09c770da032b91a150e78ffb17e069d52ae99996
Flobul <46993341+Flobul@users.noreply.github.com>
2020-01-08 14:13:12 +0100

    Update health.php

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -44,16 +44,7 @@ foreach ($eqLogics as $eqLogic) {
 		$status = '<span class="label label-danger" style="font-size : 1em; cursor : default;">{{NOK}}</span>';
 	}
 	echo '<td>' . $status . '</td>';
-
-	$powerstatuscmd = $eqLogic->getCmd('info', 'Powerstatus');
-		log::add('Optoma', 'debug', 'Power = ' . $powerstatuscmd);
-	$Powerstatus = '';
-	if (is_object($powerstatuscmd)) {
-		$Powerstatus = $powerstatuscmd->execCmd();
-	}
-		log::add('Optoma', 'debug', 'Powerstatus = ' . $signal);
-
-	
+	log::add('Optoma', 'debug', 'Power = ' . $eqLogic->getCmd('info', 'Powerstatus'));
 	echo '<td><span class="label" style="font-size : 0.9em;cursor:default;padding:0px 5px;background-color:#cccc00">' . $rssicmd .'</span></td>';
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
