e7df73132626af115e78c2b36e3aa06967aaf6f2
Flobul <46993341+Flobul@users.noreply.github.com>
2020-01-08 13:52:00 +0100

    Update health.php

--- desktop/modal/health.php
+++ desktop/modal/health.php
@@ -44,7 +44,13 @@ foreach ($eqLogics as $eqLogic) {
 		$status = '<span class="label label-danger" style="font-size : 1em; cursor : default;">{{NOK}}</span>';
 	}
 	echo '<td>' . $status . '</td>';
-	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('Powerstatus') . '</span></td>';
+
+	$remotes = Optoma::all();
+	foreach ($remotes as $remote){
+		$name = $remote->getName();
+		$rssicmd = $eqLogic->getCmd('info', 'Powerstatus' . $name);
+	}
+	echo '<span class="label" style="font-size : 0.9em;cursor:default;padding:0px 5px;background-color:#cccc00">' . $rssicmd .'</span>';
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
 	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
 }
